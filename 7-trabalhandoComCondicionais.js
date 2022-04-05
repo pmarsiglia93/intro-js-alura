e75a47b91a76192ca7055a7ca88f6fdf20c7c4d1
Paulo <pmarsiglia93@gmail.com>
2022-04-05 18:10:09 -0300

    @pmarsiglia93 [TASK: ATUALIZANDO]

--- 7-trabalhandoComCondicionais.js
+++ 7-trabalhandoComCondicionais.js
@@ -8,21 +8,26 @@ const listaDeDestinos = new Array(
     `Recife`,
 );
 
-const idadeComprador = 17;
-const estaAcompanhada = true;
+const idadeComprador = 18;
+const estaAcompanhada = false;
+const temPassagemComprada = true;
 
 console.log("Destinos possíveis: ");
 console.log(listaDeDestinos);
 
-if (idadeComprador >= 18) {
-
-    console.log("Comprador maior de idade")
-    listaDeDestinos.splice(1, 1);
-} else if (estaAcompanhada == true) {
-    console.log("Comprador está acompanhado")
-    listaDeDestinos.splice(1, 1);
-} else {
+if (
+    idadeComprador >= 18 || estaAcompanhada == true) {
+    console.log("Boa viagem");
+    listaDeDestinos.splice(2, 1);
+}  else {
     console.log("Não é maior de idade e não posso vender");
+}
+
+console.log("Embarque: \n\n")
+if(idadeComprador >= 18 && temPassagemComprada){
+    console.log("Boa Viagem");
+}else{
+    console.log("Você não pode embarcar");
 }
 
 console.log(listaDeDestinos);
